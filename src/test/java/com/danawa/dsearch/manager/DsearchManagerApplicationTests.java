c32a11a7883016e09cfd80889e8d09ad3036542f
yungi <sospecial30@danawa.com>
2021-06-04 15:28:14 +0900

    Initial commit

---
+++ src/test/java/com/danawa/dsearch/manager/DsearchManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danawa.dsearch.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsearchManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
