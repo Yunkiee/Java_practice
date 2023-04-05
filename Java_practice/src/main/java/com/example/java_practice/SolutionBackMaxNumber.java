fa9e2c8e8217b1f8b91ed421d4f1cb8727b46bfa
Yunkiee <sospecial30@danawa.com>
2023-04-06 08:58:09 +0900

    initial commit

---
+++ Java_practice/src/main/java/com/example/java_practice/SolutionBackMaxNumber.java
@@ -0,0 +1,11 @@
+package com.example.java_practice;
+
+public class SolutionBackMaxNumber {
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
+
+//        return answer;
+        return numbers;
+    }
+
+}
