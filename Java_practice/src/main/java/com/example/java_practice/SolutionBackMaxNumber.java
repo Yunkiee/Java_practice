30bba3118abb141570c0873c5c35aa953648fdcb
Yunkiee <sospecial30@danawa.com>
2023-04-06 10:04:56 +0900

    BackMaxNumber

--- Java_practice/src/main/java/com/example/java_practice/SolutionBackMaxNumber.java
+++ Java_practice/src/main/java/com/example/java_practice/SolutionBackMaxNumber.java
@@ -1,11 +1,25 @@
 package com.example.java_practice;
 
+
+import java.util.Stack;
+
 public class SolutionBackMaxNumber {
     public int[] solution(int[] numbers) {
-        int[] answer = {};
+        int[] answer = new int[numbers.length];
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
+                // 뒤에 있는 큰 수에 해당하는 모든 값 pop
+                answer[stack.pop()] = numbers[i];
+            }
 
-//        return answer;
-        return numbers;
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            answer[stack.pop()] = -1;
+        }
+
+        return answer;
     }
 
 }
